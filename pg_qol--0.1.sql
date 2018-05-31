a51a1f20ae2c522dbc973e7d950b8ba058dbfc5c
Ryan Price <rprice@aggio.io>
2018-05-31 10:42:38 -0500

    Improve file whitespacing.

--- pg_qol--0.1.sql
+++ pg_qol--0.1.sql
@@ -1,11 +1,17 @@
 -- Pass a date field to return a year/month field.
 -- Useful for time-series monthly aggregation or labelling
 -- Default delimiter is '-', i.e. returns '2018-05'
-CREATE OR REPLACE FUNCTION year_month(date_field date, delim varchar(3) = '-')
+CREATE OR REPLACE FUNCTION year_month (
+    date_field date,
+    delim varchar(3) = '-')
 RETURNS VARCHAR AS
 $BODY$
 BEGIN
-	RETURN (extract(year from date_field)::varchar(4) || delim || lpad(extract(month from date_field)::varchar(2), 2, '0'));
+	RETURN (
+	    extract(year from date_field)::varchar(4) ||
+	    delim ||
+	    lpad(extract(month from date_field)::varchar(2), 2, '0')
+	);
 END;
 $BODY$
-LANGUAGE 'plpgsql';
+LANGUAGE 'PLpgSQL';
